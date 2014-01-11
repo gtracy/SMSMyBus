d673dc9890de6197cf6b24fea562dd5aa3c47cc5
gtracy <gtracy@gmail.com>
2014-01-11 15:24:44 -0600

    extended size of the string system will accept via email

--- data_model.py
+++ data_model.py
@@ -2,7 +2,7 @@ from google.appengine.ext import db
 
 class PhoneLog(db.Model):
   phone       = db.StringProperty()
-  to          = db.StringProperty()
+  to          = db.StringProperty(multiline=True)
   date        = db.DateTimeProperty(auto_now_add=True)
   body        = db.StringProperty(multiline=True,indexed=False)
   smsID       = db.StringProperty(indexed=False)
