350e392f5cfd83d3124cc2249747c8974f754652
gtracy <gtracy@gmail.com>
2014-12-20 12:47:47 -0600

    ported to webapp2 and python 2.7

--- apps/gadgets/gadget.py
+++ apps/gadgets/gadget.py
@@ -1,28 +1,20 @@
 import os
-import wsgiref.handlers
 import logging
+import webapp2
 
-from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
 
 
-class MainHandler(webapp.RequestHandler):
+class MainHandler(webapp2.RequestHandler):
     def get(self, stopID=""):
       logging.debug('gadget definition request for stop %s' % stopID)
       template_values = {'stopID':stopID,
                          }
       path = os.path.join(os.path.dirname(__file__), '../../views/metro_template.xml')
       self.response.out.write(template.render(path,template_values))
-    
+
 ## end MainHandler()
 
-def main():
-  logging.getLogger().setLevel(logging.ERROR)
-  application = webapp.WSGIApplication([('/gadgets/metro/(.*).xml', MainHandler),
-                                        ],
-                                       debug=True)
-  wsgiref.handlers.CGIHandler().run(application)
-
-
-if __name__ == '__main__':
-  main()
+application = webapp2.WSGIApplication([('/gadgets/metro/(.*).xml', MainHandler),
+                                      ],
+                                     debug=True)
