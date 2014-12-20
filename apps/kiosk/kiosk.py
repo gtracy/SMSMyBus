6f5e8f7e824909972e5ebd92715c9703c93611d9
gtracy <gtracy@gmail.com>
2014-12-20 12:22:29 -0600

    added a 4x4 grid layout to the kiosk display maker

--- apps/kiosk/kiosk.py
+++ apps/kiosk/kiosk.py
@@ -1,46 +1,61 @@
 import os
-import wsgiref.handlers
 import logging
+import webapp2
 
-from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
 
 
-class MainHandler(webapp.RequestHandler):
+class MainHandler(webapp2.RequestHandler):
     def get(self, stopID=""):
-      stops = self.request.get('s').split(',')
-      if stops[0] is '':
-          # default to the mother fool's kiosk results
-          stops = ['1505','1878']
-      elif len(stops) == 1 or len(stops[1]) == 0:
-          stops.append('1878')
+      template_values = {}
+      logging.error('kiosk handler')
 
+      # this end point supports two different layouts.
+      #   side-by-side with two stops
+      #   a grid layout with four stops
+      #
+      # determine which one was requested
       directions = self.request.get('d').split(',')
-      if directions[0] is '':
-          # default to the mother fool's kios directions
-          directions = ['Eastbound','Westbound']
-      elif len(directions) == 1:
-          directions.append('unknown direction')
-          
-      logging.debug('KIOSK definition request for stops %s' % stops)
-      template_values = { 
-        'stop1':stops[0], 
-        'stop2':stops[1],
-        'direction1':directions[0],
-        'direction2':directions[1]
-      }
-      path = os.path.join(os.path.dirname(__file__), '../../views/kiosk.html')
+      stops = self.request.get('s').split(',')
+      if len(stops) is 2 or stops[0] is '':
+          if stops[0] is '':
+              # default to the mother fool's kiosk results
+              stops = ['1505','1878']
+          elif len(stops) == 1 or len(stops[1]) == 0:
+              stops.append('1878')
+
+          if directions[0] is '':
+              # default to the mother fool's kios directions
+              directions = ['Eastbound','Westbound']
+          elif len(directions) == 1:
+              directions.append('unknown direction')
+
+          logging.debug('Two column KIOSK definition request for stops %s' % stops)
+          template_values = {
+            'stop1':stops[0],
+            'stop2':stops[1],
+            'direction1':directions[0],
+            'direction2':directions[1]
+          }
+          path = os.path.join(os.path.dirname(__file__), '../../views/kiosk.html')
+          # self.response.out.write(template.render(path,template_values))
+      elif len(stops) == 4 and len(directions) == 4:
+          logging.error('Two column KIOSK definition request for stops %s' % stops)
+          logging.error(stops)
+          stop_list = []
+          for index,s in enumerate(stops):
+              stop_list.append({'stop_num' : s,'direction' : directions[index]})
+
+          template_values = { 'stops' : stop_list }
+          path = os.path.join(os.path.dirname(__file__), '../../views/kiosk-grid.html')
+      else:
+          logging.error('Invalid Kiosk request');
+          path = os.path.join(os.path.dirname(__file__), '../../views/kiosk-error.html')
+
       self.response.out.write(template.render(path,template_values))
-    
+
+
 ## end MainHandler()
 
-def main():
-  logging.getLogger().setLevel(logging.DEBUG)
-  application = webapp.WSGIApplication([('/kiosk', MainHandler),
-                                        ],
-                                       debug=True)
-  wsgiref.handlers.CGIHandler().run(application)
-
-
-if __name__ == '__main__':
-  main()
+app = webapp2.WSGIApplication([(r'/kiosk', MainHandler)],
+                              debug=True)
